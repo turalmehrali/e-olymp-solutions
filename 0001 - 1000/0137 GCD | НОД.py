6d667b5a875f192d3483539e294d9dfee3ab8a12
Tural Mehralı <turalmehrali@gmail.com>
2020-06-02 19:31:10 +0400

    Judge Python | GCD | НОД

---
+++ 0001 - 1000/0137 GCD | НОД.py
@@ -0,0 +1,17 @@
+
+from math import gcd
+
+n = input()
+ededler = [int(i) for i in input().split()]
+
+ebob = ededler[0]
+
+if len(ededler) == 2:
+    ebob = gcd(ededler[0], ededler[1])
+
+elif len(ededler) > 2:
+
+    for i in ededler[2:]:
+        ebob = gcd(ebob, i)
+
+print(ebob)
