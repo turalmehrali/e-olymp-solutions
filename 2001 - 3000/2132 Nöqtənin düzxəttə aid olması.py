ae4a3c5341068cb58f7cf64a4549b75939c9b678
Tural Mehralı <turalmehrali@gmail.com>
2021-01-11 11:40:27 +0400

    Judge Python

---
+++ 2001 - 3000/2132 Nöqtənin düzxəttə aid olması.py
@@ -0,0 +1,13 @@
+
+ededler = [int(i) for i in input().split()]
+
+netice = ededler[0] * ededler[2] + ededler[1] * ededler[3] + ededler[4]
+
+if netice == 0:
+    print("YES")
+
+else:
+    print("NO")
+
+
+
