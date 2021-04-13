67a8510d1a6b4896c2113fd186be8c4c90fcf3a0
Narmin Bilalova <82313136+narminb@users.noreply.github.com>
2021-04-13 14:17:26 +0400

    Judge Python

---
+++ 0001 - 1000/0480 Qüvvətə yüksəltmə-2 | Python.py
@@ -0,0 +1,11 @@
+def power(x,y,z):
+    if y==0:
+        return 1
+    elif y%2==0:
+        return power(((x%n)*(x%n))%n, y//2,n)%n
+    else:
+        return ((x%n)*(power(((x%n)*(x%n))%n,y//2,n))%n)%n
+
+a,b,n=map(int,input().split())
+d=power(a,b,n)
+print(d)
