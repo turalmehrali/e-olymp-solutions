74a5c498146f27eecb8f2dc8921b97bc0a43a0a5
Narmin Bilalova <82313136+narminb@users.noreply.github.com>
2021-04-17 14:58:58 +0400

    Judge Python

---
+++ 6001 - 7000/6126 Səhvlərdən qorunmuş növbə | Python.py
@@ -0,0 +1,47 @@
+class queue:
+    items=[]
+    def enqueue(self,item):
+        self.items.insert(0,item)
+    def size(self):
+        return len(self.items)
+    def front(self):
+        return self.items[len(self.items)-1]
+    def back(self):
+        return self.items[0]
+    def dequeue(self):
+         if(len(self.items)>0):
+             self.items.pop(len(self.items)-1)
+         else:
+             return False
+    def isempty(self):
+        return self.items==[]
+
+q=queue()
+emr=''
+
+while True:
+    emr=input()
+    if emr=='exit':
+        print('bye')
+        break
+    elif emr=='clear':
+        while q.size()>0:
+            q.dequeue()
+        print('ok')
+    elif emr=='front':
+        if q.isempty():
+            print('error')
+        else:
+            print(q.front())
+    elif emr=='pop':
+        if q.isempty():
+            print('error')
+        else:
+            print(q.front())
+            q.dequeue()
+    elif emr=='size':
+        print(q.size())
+    else:
+        emr,eded=emr.split()
+        q.enqueue(eded)
+        print('ok')
